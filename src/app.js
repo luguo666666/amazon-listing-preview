c56e3b6fe707b46875b56b8fdb6aebc7aa2c5de2
luguo666666 <luguo6677@gmail.com>
2026-09-11 18:03:48 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,6 +45,7 @@ function imageNode(item, className = '') {
 }
 
 function setFrame(frame, item, className = '') {
+  frame.hidden = !item;
   frame.replaceChildren();
   if (item) frame.append(imageNode(item, className));
   else frame.innerHTML = '<div class="empty-frame">暂无图片</div>';
